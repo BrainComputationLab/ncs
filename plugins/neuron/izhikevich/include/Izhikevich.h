36883e18bf9c24dbfc1de0b7570650f12ee61ea0
rvhoang <rvhoang@gmail.com>
2013-09-05 10:09:06 -0700

    Instantiator for izh. Loading of synapse plugins.

---
+++ plugins/neuron/izhikevich/include/Izhikevich.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <ncs/spec/Generator.h>
+
+struct Instantiator {
+  ncs::spec::Generator* a;
+  ncs::spec::Generator* b;
+  ncs::spec::Generator* c;
+  ncs::spec::Generator* d;
+  ncs::spec::Generator* u;
+  ncs::spec::Generator* v;
+};
--- plugins/neuron/izhikevich/source/Izhikevich.cpp
+++ plugins/neuron/izhikevich/source/Izhikevich.cpp
@@ -1,10 +1,44 @@
 #include <ncs/sim/FactoryMap.h>
 #include <ncs/sim/NeuronSimulator.h>
 
+#include "Izhikevich.h"
+
+bool set(ncs::spec::Generator*& target,
+         const std::string& parameter,
+         ncs::spec::ModelParameters* parameters) {
+  ncs::spec::Generator* generator = parameters->getGenerator(parameter);
+  target = generator;
+  if (!generator) {
+    std::cerr << "izhikevich requires " << parameter << " to be defined." <<
+      std::endl;
+    return false;
+  }
+  return true;
+}
+
+void* createInstantiator(ncs::spec::ModelParameters* parameters) {
+  Instantiator* instantiator = new Instantiator();
+  bool result = true;
+  result &= set(instantiator->a, "a", parameters);
+  result &= set(instantiator->b, "b", parameters);
+  result &= set(instantiator->c, "c", parameters);
+  result &= set(instantiator->d, "d", parameters);
+  result &= set(instantiator->u, "u", parameters);
+  result &= set(instantiator->v, "v", parameters);
+  if (!result) {
+    std::cerr << "Failed to create izhikevich generator" << std::endl;
+    delete instantiator;
+    return nullptr;
+  }
+  return instantiator;
+}
+
 extern "C" {
 
 bool load(ncs::sim::FactoryMap<ncs::sim::NeuronSimulator>* plugin_map) {
-  return true;
+  bool result = true;
+  result &= plugin_map->registerInstantiator("izhikevich", createInstantiator);
+  return result;
 }
   
 }
