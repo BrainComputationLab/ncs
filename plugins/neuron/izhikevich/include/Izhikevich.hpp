e1b05865b90235574e689f6634720995be6238b6
roger <rvhoang@gmail.com>
2013-09-10 14:53:45 -0700

    Began initializing devices.

---
+++ plugins/neuron/izhikevich/include/Izhikevich.hpp
@@ -0,0 +1,8 @@
+template<ncs::sim::DeviceType::Type MemoryType>
+bool IzhikevichSimulator<MemoryType>::
+addNeuron(void* instantiator, unsigned int seed) {
+}
+
+template<ncs::sim::DeviceType::Type MemoryType>
+bool IzhikevichSimulator<MemoryType>::initialize() {
+}
